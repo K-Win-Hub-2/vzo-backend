2a5f4d6f0a56d630873a89f6fe684d1190a3373e
Kaung Set Hein <127381301+kaung-set107@users.noreply.github.com>
2024-10-16 15:51:31 +0630

    fix

--- app/controllers/excelImportController.js
+++ app/controllers/excelImportController.js
@@ -1,34 +1,149 @@
 "use strict";
 const xlsx = require("xlsx");
-const superCategory = require("../models/superCategory");
-const itemTitle = require("../models/itemTitle");
 const items = require("../models/items");
+const ItemTitle = require("../models/itemTitle");
+const superCategoryModels = require("../models/superCategory");
+const subCategoryModels = require("../models/subCategory");
+const Brand = require("../models/brand");
+const mongoose = require("mongoose");
 
 exports.excelImport = async (req, res) => {
-  const workbook = xlsx.readFile(req.file.path);
-  const workSheet = workbook.Sheets[workbook.SheetNames[0]];
-  const data = xlsx.utils.sheet_to_json(workSheet);
-  console.log("workSheet", workSheet, "xlsx", xlsx, "data", data);
-  data.forEach(async (result) => {
-    const splitTitle = result.title.split(" (VZO)")[0];
-    const splitCategory = result.superCategory.split(" (VZO)")[0];
-    const foodTitle = await itemTitle.findOne({ name: splitTitle });
-    const foodCategory = await superCategory.findOne({ name: splitCategory });
-    console.log("this is data of relatedid", foodCategory, foodTitle);
-    await items.create({
-      name: result.name,
-      code: result.code,
-      fromUnit: Number(result.fromUnit) || 1,
-      toUnit: Number(result.toUnit) || 1,
-      totalUnit: Number(result.totalUnit) || 0,
-      currentQuantity: Number(result.currentQuantity) || 0,
-      sellingPrice: Number(result.sellingPrice) || 0,
-      purchasePrice: Number(result.purchasePrice) || 0,
-      deliveryPrice: Number(result.deliveryPrice) || 0,
-      reOrderQuantity: Number(result.reOrderQuantity) || 0,
-      relatedItemTitle: foodTitle._id || null,
-      relatedSuperCategory: foodCategory._id || null,
-    });
-  });
-  res.status(200).json({ success: true, message: "Import successful" });
+  try {
+    const workbook = xlsx.readFile(req.file.path);
+    const workSheet = workbook.Sheets[workbook.SheetNames[0]];
+    const data = xlsx.utils.sheet_to_json(workSheet);
+
+    console.log("data", data);
+
+    for (const result of data) {
+      const superCategory = result.superCategory.split(" (VZO)")[0];
+      const superID = await superCategoryModels.findOne({
+        name: superCategory,
+      });
+      const itemTitle = result.title.split(" (VZO)")[0];
+      const itemTitleID = await ItemTitle.findOne({
+        name: itemTitle,
+      });
+
+      const item_name = result.name;
+      const currentQty = result.currentQuantity;
+      const code = result.code;
+      const fromUnit = result.fromUnit;
+      const toUnit = result.toUnit;
+      const purchasePrice = result.purchase_price;
+      const sellingPrice = result.selling_price;
+      const title = result.title;
+
+      if (!superCategory) {
+        console.error(`SuperCategory not found: ${superCategoryName}`);
+        continue;
+      }
+
+      const existingItem = await items.findOne({
+        relatedItemTitle: itemTitleID._id,
+        name: item_name,
+      });
+
+      if (existingItem) {
+        await items.updateOne(
+          { _id: existingItem._id },
+          {
+            $set: {
+              code: code,
+
+              fromUnit: fromUnit,
+              toUnit: toUnit,
+              totalUnit: (currentQty * toUnit) / fromUnit,
+              currentQty: currentQty,
+              sellingPrice: sellingPrice,
+              purchasePrice: purchasePrice,
+              relatedSuperCategory: superID._id,
+              relatedItemTitle: itemTitleID._id,
+            },
+          }
+        );
+        // console.log(`Updated item: ${item_name}`);
+      } else {
+        await items.create({
+          name: item_name,
+          code: code,
+          fromUnit: fromUnit,
+          toUnit: toUnit,
+          totalUnit: (currentQty * toUnit) / fromUnit,
+          currentQty: currentQty,
+          sellingPrice: sellingPrice,
+          purchasePrice: purchasePrice,
+          relatedSuperCategory: superID._id,
+          relatedItemTitle: itemTitleID._id,
+        });
+        // console.log(`Created new item: ${item_name}`);
+      }
+    }
+
+    res.status(200).json({ success: true, message: "Import successful" });
+  } catch (error) {
+    console.error("Error in excelImport", error);
+    return res.status(500).send({ error: true, message: error.message });
+  }
+};
+
+exports.TitleExcelImport = async (req, res) => {
+  try {
+    const workbook = xlsx.readFile(req.file.path);
+    const workSheet = workbook.Sheets[workbook.SheetNames[0]];
+    const data = xlsx.utils.sheet_to_json(workSheet);
+
+    for (const result of data) {
+      const superCategory = result.superCategory.split(" (VZO)")[0];
+      const subCategory = result.subCategory.split(" (VZO)")[0];
+      const superID = await superCategoryModels.findOne({
+        name: superCategory,
+      });
+      const subID = await subCategoryModels.findOne({ name: subCategory });
+      const brand = result.brand.split(" (VZO)")[0];
+      const brandID = await Brand.findOne({ name: brand });
+      // console.log(`superID:`, superID);
+      const code = result.code;
+      const title = result.title;
+      const description = result.description;
+
+      const existingTitle = await ItemTitle.findOne({
+        name: title,
+        relatedCategory: superID._id,
+        relatedSubCategory: subID._id,
+      });
+
+      if (existingTitle) {
+        await ItemTitle.updateOne(
+          { _id: existingTitle._id },
+          {
+            $set: {
+              code: code,
+              description: description,
+              relatedBrand: brandID._id,
+              relatedCategory: superID._id,
+              relatedSubCategory: subID._id,
+              name: title,
+            },
+          }
+        );
+        console.log(`Updated title: ${title}`);
+      } else {
+        const resFinal = await ItemTitle.create({
+          code: code,
+          description: description,
+          relatedBrand: brandID._id,
+          relatedCategory: superID._id,
+          relatedSubCategory: subID._id,
+          name: title,
+        });
+        // console.log(`Created new title:`, resFinal);
+      }
+    }
+
+    res.status(200).json({ success: true, message: "Import successful" });
+  } catch (error) {
+    console.error("Error in TitleExcelImport", error);
+    return res.status(500).send({ error: true, message: error.message });
+  }
 };
--- app/routes/itemRoutes.js
+++ app/routes/itemRoutes.js
@@ -1,27 +1,37 @@
-"use strict"
+"use strict";
 
-const { listAllData, createData, dataById, updateDataById, deleteDataById } = require("../controllers/controller");
-const { excelImport } = require("../controllers/excelImportController");
+const {
+  listAllData,
+  createData,
+  dataById,
+  updateDataById,
+  deleteDataById,
+} = require("../controllers/controller");
+const {
+  excelImport,
+  TitleExcelImport,
+} = require("../controllers/excelImportController");
 const { storage } = require("../helper/fileUploader");
 const { catchError } = require("../lib/errorHandler");
-const verifyToken = require('../lib/verifyToken');
+const verifyToken = require("../lib/verifyToken");
 const { inputDataFromExcel } = require("../services/excelService");
 const multer = require("multer");
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage });
 
 module.exports = (app) => {
-    
-    app.route('/api/v1/item')
-        .get(listAllData)
-        .post(catchError(createData))
+  app.route("/api/v1/item").get(listAllData).post(catchError(createData));
 
-        
-    app.route('/api/v1/item/:id')
-        .get(catchError(dataById)) 
-        .put(catchError(updateDataById))
-        .delete(catchError(deleteDataById))
+  app
+    .route("/api/v1/item/:id")
+    .get(catchError(dataById))
+    .put(catchError(updateDataById))
+    .delete(catchError(deleteDataById));
 
-    app.route("/api/v1/excel/import")
-        .post(upload.single
-        ("excel"), catchError(excelImport))
+  app
+    .route("/api/v1/excel/import")
+    .post(upload.single("excel"), catchError(excelImport));
+
+  app
+    .route("/api/v1/excel/import-title")
+    .post(upload.single("excel"), catchError(TitleExcelImport));
 };
